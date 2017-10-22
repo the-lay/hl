91db458d7e733e4e355bb6315ea7d223952d67f3
Ilja Gubins <ilja.gubin@gmail.com>
2017-10-22 17:04:19 +0300

    Consistency between rolled-down and rolled-up search field sizes; Added placeholder for down/up button handling on search field; Added line separator between search field and results widget; Started working on results list item and separator appearance; Added basic input validation for the query;

--- base/gui/window.py
+++ base/gui/window.py
@@ -25,24 +25,34 @@ class SearchField(QLineEdit):
         # Icon
         self.iconSize = int(f_metric.ascent() / 1.1)
         self.icon = QIcon(str(AppSettings.get_resource('icons', 'search.png')))
-        self.setTextMargins(QMargins(self.iconSize + 13, 0, 0, 0))
+        self.setTextMargins(QMargins(self.iconSize + 13, 5, 0, 0))
 
         # Border
         self.setStyleSheet('border: none; /*border-radius: 5px;*/')
         # TODO borders visible only with translucent background, do I need em?
 
         # Sizes
-        self.setFixedSize(AppSettings.WIDTH, AppSettings.S_FIELD_HEIGHT)
+        # self.setFixedSize(AppSettings.WIDTH, AppSettings.S_FIELD_HEIGHT)
 
     # Overloading to draw an icon
     def paintEvent(self, event: QPaintEvent):
         super().paintEvent(event)
 
-        # TODO expose in settings whether to draw icon or not
+        # TODO: expose in settings whether to draw icon or not
         # Search icon in the field
         painter = QPainter(self)
         icon_pixmap = self.icon.pixmap(QSize(self.iconSize, self.iconSize))
-        painter.drawPixmap(8, (self.height() - self.iconSize) // 2, icon_pixmap)
+        painter.drawPixmap(8, (self.height() - self.iconSize + 5) // 2, icon_pixmap)
+
+    # Handling down/up button TODO: maybe modify tab too?
+    def keyPressEvent(self, event: QKeyEvent):
+        # TODO: proper button handling
+        if event.key() == Qt.Key_Down:
+            print('going down in results list')
+        elif event.key() == Qt.Key_Up:
+            print('going up in results list')
+        else:
+            super().keyPressEvent(event)
 
 
 class ResultsWidget(QWidget):
@@ -50,42 +60,90 @@ class ResultsWidget(QWidget):
         super().__init__()
 
         # Main layout
-        self.mainLayout = QHBoxLayout()
-        # self.mainLayout.setAlignment(Qt.AlignTop)
-        self.mainLayout.setContentsMargins(QMargins(1, 0, 1, 5))
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.mainLayout.setAlignment(Qt.AlignTop)
 
-        self.setFixedSize(AppSettings.WIDTH, AppSettings.RESULTS_HEIGHT)
+        # Separator
+        self.line = QFrame()
+        self.line.setContentsMargins(0, 0, 0, 0)
+        self.line.setFrameShape(QFrame.HLine)
+        self.line.setFrameShadow(QFrame.Sunken)
+        self.mainLayout.addWidget(self.line)
+
+        # Bottom layout
+        self.bottomLayout = QHBoxLayout()
+        self.bottomLayout.setContentsMargins(0, 0, 0, 0)
 
         # Results fields
         self.resultsList = QListWidget()
         self.resultsList.setContentsMargins(0, 0, 0, 0)
-        self.mainLayout.addWidget(self.resultsList)
-        self.mainLayout.setStretch(0, 4)
+        self.resultsList.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.resultsList.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.resultsList.setStyleSheet('QListWidget { border: none; background-color: rgb(255,0,255);}')
 
-        for i in range(10):
-            item = QListWidgetItem("Item %i" % i)
+        self.bottomLayout.addWidget(self.resultsList)
+        self.bottomLayout.setStretch(0, 4)  # 40%
+
+        # TODO Testing stuff, remove after results list appearance is finalized
+        for i in range(20):
+            item = QListWidgetItem(self.resultsList)
+            my_item = FoundItem('Item Item Item %i' % i)
+            item.setSizeHint(my_item.sizeHint())
             self.resultsList.addItem(item)
+            self.resultsList.setItemWidget(item, my_item)
 
         self.resultsList.setResizeMode(QListView.Adjust)
         self.resultsList.setSpacing(2)
 
         # Details field
         self.detailsField = QLabel('Results go here')
-        self.mainLayout.addWidget(self.detailsField)
-        self.mainLayout.setStretch(1, 6)
+        self.bottomLayout.addWidget(self.detailsField)
+        self.bottomLayout.setStretch(1, 6)  # 60%
 
+        self.mainLayout.addLayout(self.bottomLayout)
         self.setLayout(self.mainLayout)
 
     def search(self, field: str):
+        # self.resultsList.clear()
+        # for i in range(len(field)):
+        #     self.resultsList.addItem(QListWidgetItem('Item %i' % i))
+
         self.detailsField.setText(field)
+
+    # def focus
 
 
 class FoundItem(QWidget):
     # confidence allows sorting
     # provider allows to traceback
 
-    def __init__(self, confidence: int, provider: int, parent=None):
+    def __init__(self, title: str, confidence: int=0, provider: int=0, parent=None):
         super(FoundItem, self).__init__(parent)
+
+        # item layout
+        self.mainLayout = QHBoxLayout()
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+
+        # TODO elided version of title
+        # https://stackoverflow.com/questions/7381100/text-overflow-for-a-qlabel-s-text-rendering-in-qt
+        self.titleLabel = QLabel(title)
+        self.iconLabel = QLabel()
+        self.iconLabel.setPixmap(QPixmap(str(AppSettings.get_resource('', 'logo.png'))).scaledToHeight(30))
+        # TODO expose height of result items to settings
+
+        self.mainLayout.addWidget(self.iconLabel, 0)
+        self.mainLayout.addWidget(self.titleLabel, 1)
+
+        self.setLayout(self.mainLayout)
+
+
+class ItemSeparator(QWidget):
+
+    def __init__(self, title: str, parent=None):
+        super(ItemSeparator, self).__init__(parent)
+
+        self.title = title
 
 
 class AppWidget(QWidget):
@@ -192,7 +250,12 @@ class AppWidget(QWidget):
 
     # Assume user stopped typing, start searching
     def do_query(self):
-        # TODO Intermediate point, might be useful later to filter or do something, but now kind of pointless
+        # TODO more input validation
+        query = self.searchField.text()
+        if not query:
+            return
+
+        # Send data
         self.resultsWidget.search(self.searchField.text())
         print('Searching for', self.searchField.text())
 
